cf06e814fba4133f9fd57f3d2e89f0541424f6dd
polyorcet <imengine@yandex.ru>
2021-02-18 09:37:29 +0300

    Внесение правок
    Внес правки и заменил плагин gsap на anime.js

--- custom-js/script.js
+++ custom-js/script.js
@@ -1,22 +1,16 @@
-gsap.registerPlugin(ScrollTrigger);
+anime({
+  targets:'.header__title',
+  translateY: [100, 0],
+  opacity: [0, 1],
+  duration: 1000,
+  easing: 'easeInOutSine'
+});
 
-var advantage = $('.advantage'),
-    catalogInner = $('.mainpage-catalog__body'),
-    catalogImage = $('.mainpage-catalog__image');
-
-var advTl = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#advantages"
-     }
-   });
-
-   var catunitTl = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#mainpage__catalog"
-     }
-   });
- 
-
-   advTl.staggerFrom(advantage, 1, {opacity: 0.6, y: 100, duration: 1, ease: "slow"}, 0.2);
-
-   gsap.from($('.header__title'), 1, {opacity: 0.6, y: 50, duration: 1, ease: "slow"}, 0.2);
+anime({
+  targets:'.mainpage-catalog__title',
+  translateY: [60, 0],
+  opacity: [0, 1],
+  duration: 1000,
+  delay: anime.stagger(300),
+  easing: 'easeInOutSine'
+});
