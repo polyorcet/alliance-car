0ab1057f2a724b64ab0dca645675b8730a3b9cba
polyorcet <imengine@yandex.ru>
2021-02-26 06:59:51 +0300

    Доработка страницы автомобиля

--- custom-js/script.js
+++ custom-js/script.js
@@ -15,3 +15,43 @@ anime({
   easing: 'easeInOutSine'
 });
 
+
+$(document).ready(function(){
+  $('.car-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    autoplay:true,
+    items:3,
+    responsiveClass:true,
+    navText:["<div class='nav-btn prev-slide'><img src='/img/prev-slide.svg'></div>","<div class='nav-btn next-slide'><img src='/img/next-slide.svg'></div>"],
+    responsive : {
+      0 : {
+        items:1,
+        nav:false,
+        dots:false
+      },
+      768 : {
+        items:2,
+        nav:false,
+        dots:false
+      },
+      992 : {
+        items:3,
+        nav:true,
+        dots:false
+      }
+  }
+});
+});
+
+$(document).ready(function(){
+  var mwidth = $(".car-page__title").width();
+  $(".car-title__sub").width(mwidth);
+  $(".header__car-price").width(mwidth);
+});
+
+$(window).resize(function(){
+  var mwidth = $(".car-page__title").width();
+  $(".car-title__sub").width(mwidth);
+  $(".header__car-price").width(mwidth);
+});
